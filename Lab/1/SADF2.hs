d3fc141bfa522912f82f033847933cadab74d7b1
Lonely52Hz <1440279963@qq.com>
2022-02-09 15:59:04 +0100

    Update

--- Lab/1/SADF2.hs
+++ Lab/1/SADF2.hs
@@ -17,6 +17,6 @@ d = detector11SADF consume_rate next_state
     k_scenario_1 = (1, (0, 1), \[x] -> ([], [-x]))
     select_scenario 0 = (1, [k_scenario_0])
     select_scenario 1 = (1, [k_scenario_1])
-    initial_state = 0
+    initial_state = 1
 
 test_s = signal [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
